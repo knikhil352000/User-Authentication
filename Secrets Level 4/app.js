f45c7ab3d00a8e443a06448ed43006fe733a4e2e
Kumar Nikhil <knikhil352000@gmail.com>
2020-11-06 22:50:03 +0530

    Level 5: Added passport.js for sessions

---
+++ Secrets Level 4/app.js
@@ -0,0 +1,79 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const app = express();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+app.use(express.static('public'));
+mongoose.connect('mongodb://localhost:27017/userDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+
+
+const User = new mongoose.model('User', userSchema);
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/login', function (req, res) {
+    res.render('login');
+});
+
+app.get('/register', function (req, res) {
+    res.render('register');
+});
+
+app.post('/register', function (req, res) {
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('secrets');
+            }
+        });
+    })
+    // const newUser = new User({
+    //     email: req.body.username,
+    //     password: md5(req.body.password)
+    // });
+
+});
+
+app.post('/login', function (req, res) {
+    const username = req.body.username;
+    const password = (req.body.password);
+    User.findOne({ email: username }, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                bcrypt.compare(password, foundUser.password, function (err, result) {
+                    if (result === true) {
+                        res.render('secrets');
+                    }
+                })
+            }
+        }
+    });
+});
+app.listen(3000, function () {
+    console.log('Server is running at port 3000');
+});
